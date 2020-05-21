cdd718d86f7afd0d6f90142eab6c1eb1022debdb
ogg17 <sewa200018@gmail.com>
2020-05-21 23:17:29 +0300

    init repository

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+[RequireComponent(typeof(Image))]
+public class Button : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private Image image;
+    [SerializeField] private UnityEvent clickEvent;
+    [SerializeField] private Color clickColor;
+    [SerializeField] private float time;
+
+    private bool isDown = false;
+    private Color defaultColor;
+
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        defaultColor = image.color;
+    }
+
+    private void Update()
+    {
+        if (!isDown) image.color = Color.Lerp(image.color, defaultColor, time * Time.deltaTime);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        clickEvent.Invoke();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isDown = true;
+        image.color = clickColor;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isDown = false;
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
